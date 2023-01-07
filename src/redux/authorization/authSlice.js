21e44ea4d8fb0a1c2d899cc1d97a99e353719968
Fyl Andrii <fil.andrey91@gmail.com>
2023-01-07 16:37:43 +0200

    adds styles for HomeView

--- src/redux/authorization/authSlice.js
+++ src/redux/authorization/authSlice.js
@@ -37,7 +37,6 @@ const authSlice = createSlice({
 
         },
         [logOut.fulfilled](state) {
-            console.log(state);
             // скидаємо стейт до початкових значень
             state.user = { name: null, email: null };
             state.token = null;
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,6 +1,7 @@
 import css from './Common.module.css'
 import { useSelector } from "react-redux";
 import { authSelectors } from "redux/authorization/authSelectors";
+import { NavLink } from 'react-router-dom';
 
 export const HomeView = () => {
     const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
@@ -10,8 +11,17 @@ export const HomeView = () => {
                 className={css.home__grettings}>
                 Use Phonebook application !
             </p> :
-                <p className={css.home__grettings}> Glad to see you in Phonebook application! <br />
-                    <span className={css.suggestion__register}>Please login or register...</span></p>}
+                <div className={css.home__grettings}> 
+                Glad to see you in Phonebook application! <br />
+                    <div className={css.suggestion__register}>
+                        Please, {''}  
+                        <NavLink
+                            className={css.reference__route} to="/login">login</NavLink> {''} 
+                        or {''} 
+                        <NavLink
+                            className={css.reference__route} to="/register">register</NavLink></div>
+                </div>}
+                    
            
         </div>
     )
